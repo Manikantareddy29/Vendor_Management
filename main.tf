f0dbb237ca5d7a6a506c750c6fb67ed400c6d04e
Manikantareddy29 <142870795+Manikantareddy29@users.noreply.github.com>
2024-12-04 15:22:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,3 +73,30 @@ module "linux_virtual_machine" {
     caching              = "ReadWrite"
   }
 }
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-123"
+  acl    = "private"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Security group for example"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
